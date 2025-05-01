d179b69fba76274b8dd43413385bf29d686046ab
Jelle Jacobs <jelle.jacobs@gmail.com>
2025-05-01 16:46:15 +0200

    at page 82

--- ast/ast.go
+++ ast/ast.go
@@ -34,6 +34,11 @@ type Program struct {
     Statements []Statement
 }
 
+type Boolean struct {
+    Token token.Token
+    Value bool
+}
+
 func (p *Program) String() string {
     var out bytes.Buffer
     for _, s := range p.Statements {
@@ -95,11 +100,25 @@ type Identifier struct {
     Value string
 }
 
+type PrefixExpression struct {
+    Token token.Token
+    Operator string
+    Right Expression
+}
+
+type InfixExpression struct {
+    Token token.Token
+    Left Expression
+    Operator string
+    Right Expression
+}
+
 func (ls *LetStatement) statementNode()     {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (i *Identifier) expressionNode()       {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string { return i.Value }
 
 func (rs *ReturnStatement) statementNode()      {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
@@ -107,9 +126,34 @@ func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 func (es *ExpressionStatement) statementNode()      {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
-func (i *Identifier) String() string { return i.Value }
 
 func (il *IntegerLiteral) expressionNode()       {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string { return il.Token.Literal }
 
+func (pe *PrefixExpression) expressionNode()        {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+    var out bytes.Buffer
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
+    return out.String()
+}
+
+func (oe *InfixExpression) expressionNode()        {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+    var out bytes.Buffer
+    out.WriteString("(")
+    out.WriteString(oe.Left.String())
+    out.WriteString(" " + oe.Operator + " ")
+    out.WriteString(oe.Right.String())
+    out.WriteString(")")
+    return out.String()
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string { return b.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -19,6 +19,17 @@ const (
     CALL
 )
 
+var precedences = map[token.TokenType]int{
+    token.EQ: EQUALS,
+    token.NOT_EQ: EQUALS,
+    token.LT: LESSGREATER,
+    token.GT: LESSGREATER,
+    token.PLUS: SUM, 
+    token.MINUS: SUM,
+    token.ASTERISK: PRODUCT,
+    token.SLASH : PRODUCT,
+}
+
 type (
     prefixParseFn func() ast.Expression
     infixParseFn func(ast.Expression) ast.Expression
@@ -41,9 +52,23 @@ func New(l *lexer.Lexer) *Parser {
         errors: []string{},
     }
 
+    p.infixParseFns = make(map[token.TokenType]infixParseFn)
+    p.registerInfix(token.PLUS, p.parseInfixExpression)
+    p.registerInfix(token.MINUS, p.parseInfixExpression)
+    p.registerInfix(token.SLASH, p.parseInfixExpression)
+    p.registerInfix(token.ASTERISK, p.parseInfixExpression)
+    p.registerInfix(token.EQ, p.parseInfixExpression)
+    p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
+    p.registerInfix(token.LT, p.parseInfixExpression)
+    p.registerInfix(token.GT, p.parseInfixExpression)
+
     p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.BANG, p.parsePrefixExpression)
+    p.registerPrefix(token.MINUS, p.parsePrefixExpression)
     p.registerPrefix(token.IDENT, p.parseIdentifier)
     p.registerPrefix(token.INT, p.parseIntegerLiteral)
+    p.registerPrefix(token.TRUE, p.parseBoolean)
+    p.registerPrefix(token.FALSE, p.parseBoolean)
     p.nextToken()
     p.nextToken()
     return p
@@ -102,6 +127,10 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
     return stmt
 }
 
+func (p *Parser) parseBoolean() ast.Expression {
+    return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
 func (p *Parser) parseLetStatement() *ast.LetStatement {
     stmt := &ast.LetStatement{Token: p.curToken}
     
@@ -134,9 +163,19 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 func (p *Parser) parseExpression(precedence int) ast.Expression {
     prefix := p.prefixParseFns[p.curToken.Type]
     if prefix == nil {
+        p.noPrefixParseFnError(p.curToken.Type)
         return nil
     }
     leftExp := prefix()
+
+    for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
+        infix := p.infixParseFns[p.peekToken.Type]
+        if infix == nil {
+            return leftExp
+        }
+        p.nextToken()
+        leftExp = infix(leftExp)
+    }
     return leftExp
 }
 
@@ -181,3 +220,50 @@ func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
 func (p *Parser) parseIdentifier() ast.Expression {
     return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+    msg := fmt.Sprintf("no prefix parse function for %s found", t)
+    p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+    expression := &ast.PrefixExpression{
+            Token: p.curToken,
+            Operator: p.curToken.Literal,
+    }
+    
+    p.nextToken()
+    
+    expression.Right = p.parseExpression(PREFIX)
+    
+    return expression
+}
+
+func (p *Parser) peekPrecedence() int {
+    if p, ok := precedences[p.peekToken.Type]; ok {
+        return p
+    }
+    return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+    if p, ok := precedences[p.curToken.Type]; ok {
+        return p
+    }
+    return LOWEST
+}
+
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+    expression := &ast.InfixExpression{
+            Token: p.curToken,
+            Operator: p.curToken.Literal,
+            Left: left,
+    }
+    
+    precedence := p.curPrecedence()
+    p.nextToken()
+    expression.Right = p.parseExpression(precedence)
+    
+    return expression
+}
+
